7d334e3fb8c02189ac40deaebf06c195aa5c9160
orioncrespo10 <96392837+orioncrespo10@users.noreply.github.com>
2022-01-24 17:12:23 -0600

    Update About.svelte

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -1,30 +1,6 @@
-<!-- About.svelte -->
-<head>
-<script>
-  import { Router, Link, Route } from "svelte-routing";
-  import Home from "./routes/Home.svelte";
-  import About from "./routes/About.svelte";
-
-  export let url = "";
-</script>
-
-<Router url="{url}">
-  <nav>
-    <Link to="/">Home</Link>
-    <Link to="about">About</Link>
-  </nav>
-  <div>
-    <Route path="about" component="{About}" />
-    <Route path="/"><Home /></Route>
-  </div>
-</Router>
-</head>
-
-<body>
-
-<p>I am normal</p>
-<p style="color:red;">I am red</p>
-<p style="color:blue;">I am blue</p>
-<p style="font-size:50px;">I am big</p>
-
-</body>
+<h1>
+	This is About
+</h1>
+<p>
+	Imagine a world where world is not world!
+</p>
