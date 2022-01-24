345b8cc371af6264dfd787ef11319ccd101c60a4
orioncrespo10 <96392837+orioncrespo10@users.noreply.github.com>
2022-01-24 14:32:29 -0600

    Update home.svelte

---
+++ src/routes/home.svelte
@@ -0,0 +1,17 @@
+<script>
+    const currentDate = new Date();
+</script>
+<main>
+    <h1>Welcome to Home Page</h1>
+    {currentDate}
+</main>
+<style>
+    .main {
+        text-align: center;
+    }
+    h1 {
+        text-transform: uppercase;
+        font-size: 3em;
+        font-weight: 100;
+    }
+</style>
